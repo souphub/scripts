c4fb9ff4430d4558e19c8564bbd68756bf67ef01
andrewtarr <andrewtarr@gmx.net>
2015-12-05 15:25:17 +1300

    slight wording change in three-minutes-left.sh

--- three-minutes-left.sh
+++ three-minutes-left.sh
@@ -3,4 +3,4 @@
 
 
 (sleep 1 && /usr/bin/wmctrl -F -a "3 Minute Warning" -b add,above) &
-(/usr/bin/zenity --warning --title="3 Minute Warning" --text='<span font="20">You have 3 minutes left. \n\n After 3 minutes, you will be logged out. \n\n You must save anything you want to keep <b>now</b>.</span>')
+(/usr/bin/zenity --warning --title="3 Minute Warning" --text='<span font="20">You have 3 minutes left. \n\n After 3 minutes, you will be logged out. \n\n Save anything you want to keep <b>now</b>.</span>')
