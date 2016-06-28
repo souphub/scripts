e17d94447225b5fb74bae6e22d907e9a18d443e0
Soup Hub <souphub@gmail.com>
2016-06-28 13:12:28 +1200

    add saned server configs

--- setup_network_scanner.sh
+++ setup_network_scanner.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # this should do it: 
-# https://help.ubuntu.com/community/SaneDaemonTutorial
+echo "Copy the configs in to place" 
+echo "See: https://help.ubuntu.com/community/SaneDaemonTutorial"
+sudo cp -v saned.conf /etc/sane.d/saned.conf
+sudo cp -v saned /etc/default/saned
+# restart service
