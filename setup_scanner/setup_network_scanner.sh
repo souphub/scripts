aef3496d316286e6158ca896e3838908f1a957bb
Hub Admin <souphub@gmail.com>
2021-07-12 15:50:41 +1200

    some things to do after 20.04 upgrade

--- setup_scanner/setup_network_scanner.sh
+++ setup_scanner/setup_network_scanner.sh
@@ -7,7 +7,8 @@
 # restart service
 # ^ that was for old scanner we have a different one now
 # New printer setup script: 
-#PRINTER_IP=172.16.3.164
+PRINTER_IP=172.16.3.164
 # install:
-# dpkg -i brscan4-0.4.9-1.amd64.deb
-# sudo brsaneconfig4 -a name=DCP7065DN model=DCP7065DN ip=$PRINTER_IP
+dpkg -i brscan4-0.4.9-1.amd64.deb
+sudo brsaneconfig4 -a name=DCP7065DN model=DCP7065DN ip=$PRINTER_IP
+# Just rerun this after 20.04 upgrade
